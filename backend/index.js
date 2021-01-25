af0d8e8588ef09122b353e287833b0864a5264ee
nisaelek <nisaelek@github.com>
2021-01-25 22:07:04 +0300

    changed some features

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('recieve-color',color);
   })
 });
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+const port=process.env.PORT;
+http.listen(port, () => {
+  console.log('listening on *:',port);
 });
